dbfd01d3d2d7e128affea8766b9c2a9be6b3fc3b
Alonso Parra <jose.alonso.parra.vargas@gmail.com>
2024-02-22 16:58:13 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        chunkSizeWarningLimit: 2000
-    },
     plugins: [
         laravel({
             input: [
@@ -14,4 +11,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+    },
 });
