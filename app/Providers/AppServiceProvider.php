35dcac2c8a48f481fa3ca6c3428489320c3028dd
jromero <eg2bourne@gmail.com>
2024-03-13 15:14:42 -0600

    Mercado pago

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,10 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
-use Illuminate\Support\Facades\Schema;
+//use Illuminate\Support\Facades\Schema;
 
 /**
  * Bootstrap any application services.
