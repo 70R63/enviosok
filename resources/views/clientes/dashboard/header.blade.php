10b4719e239f453712923874c76cd228457182b0
jromero\JORGE <eg2bourne@gmail.com>
2024-07-02 16:39:00 -0600

    Destinatarios

--- resources/views/clientes/dashboard/header.blade.php
+++ resources/views/clientes/dashboard/header.blade.php
@@ -3,6 +3,7 @@
         <h2 class="main-content-title tx-24 mg-b-5">DESTINATARIOS</h2>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ route('dashboard') }} ">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('clientes.index') }} ">Destinatarios</a></li>
             <li class="breadcrumb-item active" aria-current="page">DESTINATARIOS</li>
         </ol>
     </div>
