df51bcdf5d7fb4141470027acbe43420c39b5599
jromero\JORGE <eg2bourne@gmail.com>
2024-07-09 02:07:19 -0600

    Ajuste para facturación

--- app/Dto/Misfinanzas/FacturaExterna.php
+++ app/Dto/Misfinanzas/FacturaExterna.php
@@ -61,7 +61,7 @@ class FacturaExterna
         
         //Datos solo se usaran en produccion, para LOCAL y DEV se tiene que usar datos entregaedos po el proveedor
         $data['rfcEmisor']= "CXM201015UN0";
-        $data['razon_social']= "COMERCIALIZADORA XPERTA MEXICO SA DE CV";
+        $data['razon_social']= "COMERCIALIZADORA XPERTA MEXICO";
         $data['regimen_fiscal']= "601";
         $data['cp']="11320";
         Log::info($this->numeroDeSolicitud." ".__CLASS__." ".__FUNCTION__." ".__LINE__);
