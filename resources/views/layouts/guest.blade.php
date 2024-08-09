e764a5577612204b2b16a1a435088744aba7e0a6
Alonso Parra <jose.alonso.parra.vargas@gmail.com>
2024-08-08 17:34:58 -0700

    se agregan tags en el head

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,8 +20,36 @@
         gtag('js', new Date());
 
         gtag('config', 'AW-16657588228');
+    </script><script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5087677,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+<!-- Event snippet for Vista de página conversion page -->
+    <script>
+        gtag('event', 'conversion', {
+            'send_to': 'AW-16657588228/I2TyCMOPzcYZEITA-oY-',
+            'value': 1.0,
+            'currency': 'MXN'
+        });
+    </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16657588228"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-16657588228');
     </script>
 </head>
+
+
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
